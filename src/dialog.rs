278c45196f2f725c5f86b1118fd396745e5a9983
Kaamaru <160567890+Kaamaru@users.noreply.github.com>
2025-01-25 23:00:35 +0700

    V1.1
    1.X??? What about the previous vers?? 0.X??
    
    Added:
    L/R , Wheel:{Up/Down} to Scroll:{Up/Down} (Only at the text box)

--- src/dialog.rs
+++ src/dialog.rs
@@ -1,3 +1,4 @@
+use imagesize::size as getsize;
 use macroquad::input::*;
 use macroquad::prelude::*;
 use macroquad::time::*;
@@ -6,6 +7,7 @@ use macroquad::ui::{
     widgets::{self, Group},
     Drag, Skin, Ui,
 };
+use miniquad::window::blocking_event_loop;
 use serde::Deserialize;
 use serde_json::Result;
 use std::collections::HashMap;
@@ -14,7 +16,6 @@ use std::fmt::Debug;
 use std::fmt::Formatter;
 use std::fs::File;
 use std::io::Read;
-use imagesize::size as getsize;
 
 use crate::DIALOGVISIBILITY;
 #[derive(Deserialize, Debug)]
@@ -31,23 +32,17 @@ struct Dialog {
     dialog: Vec<Part>,
 }
 
-async fn tsleep(t: f32) {
-    let mut a = 0.0;
-    while a < t {
-        a += get_frame_time();
-    }
-}
-
 async fn get_pos(x: i32, y: i32) -> Vec2 {
     let w = screen_width();
     let h = screen_height();
+    let char_w: f32 = w * (20.0 / 100.0);
     vec2(
         match x {
             -1 => w * 25.0 / 100.0,
             0 => w * 50.0 / 100.0,
             1 => w * 75.0 / 100.0,
             _ => w * 50.0 / 100.0,
-        },
+        } - (char_w / 2.0),
         match y {
             0 => h * 40.0 / 100.0,
             1 => h * 60.0 / 100.0,
@@ -57,25 +52,64 @@ async fn get_pos(x: i32, y: i32) -> Vec2 {
     )
 }
 
-// async fn get_textureparams  (
-//     img : &str,
-// )-> DrawTextureParams{
+async fn get_textureparams(img: &str) -> DrawTextureParams {
+    let sizevec = match getsize(img) {
+        Ok(dim) => vec2(dim.width as f32, dim.height as f32),
+        Err(why) => vec2(0.0, 0.0),
+    };
 
-//     match getsize(img) {
-//         Ok(dim) => {
-//             assert_eq!(dim.width, 716);
-//             assert_eq!(dim.height, 716);
-//         }
-//         Err(why) => println!("Error getting size: {:?}", why)
-//     }
+    let char_w: f32 = screen_width() * (20.0 / 100.0);
+    DrawTextureParams {
+        dest_size: Some(vec2(char_w, char_w * (sizevec.y / sizevec.x))),
+        ..Default::default()
+    }
+}
 
-//     DrawTextureParams {
-//         dest_size: Some(vec2(300.0, 300.0)),
-//         ..Default::default()
-//     };
+fn load_parts<'a>(
+    dialogindex: &mut i32,
+    txtinp1: &'a mut String,
+    mut temp1: &'a mut String,
+    txtrend1: &mut String,
+    txtrend2: &mut String,
+    title1: &mut String,
+    active_char_x: &mut i32,
+    active_char_y: &mut i32,
+    active_char_id: &mut i32,
+    active_char: &mut String,
+    active_part: &mut String,
+) {
+    let mut file = File::open("dialog.json").expect("CAN'T OPEN FILE YO!!??");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("CAN'T READ FILE BRUHH??");
 
-// }
+    let dialog: Dialog = serde_json::from_str(&contents).expect("CAN'T PARSE FILE DUDE!!??");
 
+    temp1 = txtinp1;
+
+    if let Some(dialog_part) = dialog.dialog.get(*dialogindex as usize) {
+        println!("{:?}", dialog_part);
+        *active_char_x = dialog_part.posx;
+        *active_char_y = dialog_part.posy;
+        *active_char_id = dialog_part.id;
+        *active_char = dialog_part.character.clone();
+        *active_part = dialog_part.text.clone();
+
+        *title1 = active_char.clone();
+        *txtrend2 = active_part.clone();
+    } else {
+        println!("Index out of bounds: {}", dialogindex);
+        *dialogindex = 0;
+    }
+}
+
+pub fn prev_dialog(dialogindex: &mut i32) {
+    *dialogindex += -1;
+}
+
+pub fn next_dialog(dialogindex: &mut i32) {
+    *dialogindex += 1;
+}
 
 pub async fn run_dialog() {
     let mut file = File::open("dialog.json").expect("CAN'T OPEN FILE YO!!??");
@@ -100,43 +134,52 @@ pub async fn run_dialog() {
     let mut txtrend1 = String::new();
     let mut txtrend2 = String::new();
     let mut title1 = String::new();
-    let mut dialogindex: usize = 0;
+    let mut dialogindex: i32 = 0;
+    let mut mos_in_dialog: bool = false;
+
+    let wheel_prev : f32= 0.0;
+    let wheel_next :f32 = 0.0;
+
 
     //Files I think
     //Add HashMap<Name from path , path.png>
-    let mut paths : HashMap<usize, &str> = HashMap::new();
+    let mut imgpaths: HashMap<String, String> = HashMap::new();
 
     let path = ".";
     let entries = std::fs::read_dir("assets/").unwrap();
-        for entry in entries {
-            match entry {
-                Ok(entry) => {
-                    let filename1 = match entry.file_name().into_string() {
-                        Ok(k) => k,
-                        Err(e) => panic!("{:?}",e),
-                    };
-                    println!("Processing entry: {}", filename1.trim_end_matches(".png")  );
-                    // path.insert(,fotmat!(entry))
-                }
-                Err(e) => {
-                    println!("  entry error: {:?}", e);
-                }
+    for entry in entries {
+        match entry {
+            Ok(entry) => {
+                let mut filename1 = match entry.file_name().into_string() {
+                    Ok(k) => k,
+                    Err(e) => panic!("{:?}", e),
+                };
+                filename1 = filename1.trim_end_matches(".png").to_string();
+                // println!("Processing entry: {}", filename1.trim_end_matches(".png"));
+                imgpaths.insert(
+                    filename1.clone().to_uppercase(),
+                    format!("assets/{}.png", filename1),
+                );
+                println!("{:?}", imgpaths);
             }
-        }
+            Err(e) => {
+                println!("  entry error: {:?}", e);
+            }
+        };
+    }
 
-
-    let C1: Texture2D = load_texture("assets/Charlotte.png").await.unwrap();
-    let C2: Texture2D = load_texture("assets/Ferris.png").await.unwrap();
+    let c1: Texture2D = load_texture("assets/Charlotte.png").await.unwrap();
+    let c2: Texture2D = load_texture("assets/Ferris.png").await.unwrap();
 
     // do scale HERE
-    
+
     let mut active_char_id: i32 = 0;
     let mut active_char: String = String::new();
     let mut active_part: String = String::new();
     let mut active_char_x: i32 = 0;
     let mut active_char_y: i32 = 0;
 
-    let skin = {
+    let skin1 = {
         let label_style = root_ui()
             .style_builder()
             .font(include_bytes!("../htowert.ttf"))
@@ -145,13 +188,21 @@ pub async fn run_dialog() {
             .font_size(30)
             .build();
 
+        let button_style = root_ui()
+            .style_builder()
+            // .margin(RectOffset::new(10.0, 10.0, 0.0, 0.0))
+            .text_color(Color::from_rgba(180, 180, 100, 255))
+            .font_size(40)
+            .build();
+
         Skin {
             label_style,
+            button_style,
             ..root_ui().default_skin()
         }
     };
 
-    let mut win_skin = skin.clone();
+    let mut win_skin = skin1.clone();
 
     loop {
         clear_background(WHITE);
@@ -169,70 +220,111 @@ pub async fn run_dialog() {
                 temp1 = tail.clone();
             }
 
+            let mut dialogboxpos: Vec2 = vec2(
+                screen_width() * (boxsize.x / 100.0),
+                screen_height() - (screen_height() * (boxsize.x / 100.)) - boxsize.y,
+            );
+            let mut dialogboxsize: Vec2 = vec2(
+                screen_width() - 2.0 * (screen_width() * (boxsize.x / 100.)),
+                boxsize.y,
+            );
+
+            info!("{:?}", mouse_position());
+
+            let boxposrange_x = dialogboxpos.x..(dialogboxpos.x + dialogboxsize.x);
+            let boxposrange_y = dialogboxpos.y..(dialogboxpos.y + boxsize.y);
+
+            if boxposrange_x.contains(&mouse_position().0) {
+                info!("X");
+            }
+            if boxposrange_y.contains(&mouse_position().1) {
+                info!("Y");
+            }
+
             root_ui().push_skin(&win_skin);
-            widgets::Window::new(
-                hash!(),
-                vec2(
-                    screen_width() * (boxsize.x / 100.0),
-                    screen_height() - (screen_height() * (boxsize.x / 100.)) - boxsize.y,
-                ),
-                vec2(
-                    screen_width() - 2.0 * (screen_width() * (boxsize.x / 100.)),
-                    boxsize.y,
-                ),
-            )
-            .label(title1.clone().as_str())
-            .titlebar(false)
-            .movable(false)
-            .close_button(true)
-            .ui(&mut *root_ui(), |ui| {
-                ui.label(None, &txtrend2);
-            });
+
+            mos_in_dialog = if boxposrange_x.contains(&mouse_position().0)
+                && boxposrange_y.contains(&mouse_position().1)
+            {
+                true
+            } else {
+                false
+            };
+            info!("{}", mos_in_dialog);
+
+            widgets::Window::new(hash!(), dialogboxpos, dialogboxsize)
+                .label(title1.clone().as_str())
+                .titlebar(false)
+                .movable(false)
+                .close_button(true)
+                .ui(&mut *root_ui(), |ui| {
+                    ui.label(None, &txtrend2);
+                });
 
             root_ui().pop_skin();
         }
-
-        widgets::Window::new(hash!(), vec2(0.0, 0.0), vec2(300.0, 300.0))
-            .label("Dia input")
-            .movable(true)
-            .ui(&mut *root_ui(), |ui| {
-                ui.label(None, "Insert Dialoge");
-                ui.editbox(hash!(), vec2(250., 150.), &mut txtinp1);
-
-                if ui.button(None, "Send") {
-                    temp1 = txtinp1.clone();
-                    txtrend1.clear();
-
-                    if let Some(dialog_part) = dialog.dialog.get(dialogindex) {
-                        println!("{:?}", dialog_part);
-                        active_char_x = dialog_part.posx.clone();
-                        active_char_y = dialog_part.posy.clone();
-                        active_char_id = dialog_part.id.clone();
-                        active_char = dialog_part.character.clone();
-                        active_part = dialog_part.text.clone();
-
-                        title1 = active_char.clone();
-                        txtrend2 = active_part.clone();
-
-                        dialogindex += 1;
-                    } else {
-                        println!("Index out of bounds: {}", dialogindex);
-                        dialogindex = 0;
-                    }
-                }
-            });
         // UI done.
 
-        //int
+        if mos_in_dialog && (is_mouse_button_pressed(MouseButton::Left) || mouse_wheel().1 >0.0) {
+            // next_dialog(&mut dialogindex);
+            info!("next!");
+            load_parts(
+                &mut dialogindex,
+                &mut txtinp1,
+                &mut temp1,
+                &mut txtrend1,
+                &mut txtrend2,
+                &mut title1,
+                &mut active_char_x,
+                &mut active_char_y,
+                &mut active_char_id,
+                &mut active_char,
+                &mut active_part,
+            );
+            // dialogindex += 1;
+            next_dialog(&mut dialogindex);
+        }
+        if mos_in_dialog && (is_mouse_button_pressed(MouseButton::Right) || mouse_wheel().1 <0.0) {
+            // next_dialog(&mut dialogindex);
+            info!("next!");
+            load_parts(
+                &mut dialogindex,
+                &mut txtinp1,
+                &mut temp1,
+                &mut txtrend1,
+                &mut txtrend2,
+                &mut title1,
+                &mut active_char_x,
+                &mut active_char_y,
+                &mut active_char_id,
+                &mut active_char,
+                &mut active_part,
+            );
+            // dialogindex += 1;
+            prev_dialog(&mut dialogindex);
+        }
 
         let mut activepos: Vec2 = get_pos(active_char_x, active_char_y).await;
 
-        // match active_char_id {
-        //     // 1 => draw_texture_ex(&C1, activepos.x, activepos.y, WHITE, get_textureparams()),
-        //     // 2 => draw_texture_ex(&C2, activepos.x, activepos.y, WHITE, get_textureparams()),
+        match active_char_id {
+            1 => draw_texture_ex(
+                &c1,
+                activepos.x,
+                activepos.y,
+                WHITE,
+                get_textureparams(imgpaths.get("CHARLOTTE").expect("Failed to Draw")).await,
+            ),
 
-        //     _ => info!("None??!"),
-        // }
+            2 => draw_texture_ex(
+                &c2,
+                activepos.x,
+                activepos.y,
+                WHITE,
+                get_textureparams(imgpaths.get("FERRIS").expect("Failed to Draw")).await,
+            ),
+
+            _ => info!("None??!"),
+        }
 
         next_frame().await;
     }
